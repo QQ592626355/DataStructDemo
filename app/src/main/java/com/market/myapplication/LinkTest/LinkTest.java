e25cdb7167f91aa2d4e7dc4adb21f5fc84af25f7
xulimin <592626355@qq.com>
2020-06-28 12:02:56 +0800

    增加了无环链表找交点的逻辑

--- app/src/main/java/com/market/myapplication/LinkTest/LinkTest.java
+++ app/src/main/java/com/market/myapplication/LinkTest/LinkTest.java
@@ -6,15 +6,14 @@ package com.market.myapplication.LinkTest;
  * 链表的基本练习题，包括了链表翻转，
  * 判断链表是否有环，判断环入口，判断两个链表是否相交，
  * 查找倒数第K个元素，从尾到头打印链表
- *
  */
 public class LinkTest {
     //内部类来声明结点
-    static class  Node<T>{
+    static class Node<T> {
         T value;
         Node<T> next;
 
-        public Node(T value,Node next){
+        public Node(T value, Node next) {
             this.value = value;
             this.next = next;
         }
@@ -22,9 +21,9 @@ public class LinkTest {
 
 
     //链表翻转(递归)第一个结点即为头指针
-    public static Node reverseLink(Node head){
+    public static Node reverseLink(Node head) {
         //可行性判断
-        if (head ==null ||head.next==null){
+        if (head == null || head.next == null) {
             return head;
         }
         Node temp = head.next;
@@ -33,12 +32,13 @@ public class LinkTest {
         head.next = null;
         return newHead;
     }
+
     //链表翻转(迭代)
     //使用2个指针，pre和next，next做临时变量
-    public static Node reverseLink2(Node head){
-        Node pre =null;
+    public static Node reverseLink2(Node head) {
+        Node pre = null;
         Node next = null;
-        while(head!=null){
+        while (head != null) {
             next = head.next;
             head.next = pre;
             pre = head;
@@ -46,22 +46,24 @@ public class LinkTest {
         }
         return head;
     }
+
     //判断链表是否有环
-    public boolean isCircleLink(Node head){
-        if (head==null||head.next==null){
+    public boolean isCircleLink(Node head) {
+        if (head == null || head.next == null) {
             return false;
         }
         Node slow = head;
-        Node fast =head;
-        while(fast != null){
+        Node fast = head;
+        while (fast != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if (fast == slow){
+            if (fast == slow) {
                 return true;
             }
         }
         return false;
     }
+
     //一个有环链表判断入口，从设立一个头指针，头指针和慢指针相遇的点即为入口
     public Node getInterNode(Node head) {
         //可行性判断
@@ -84,59 +86,109 @@ public class LinkTest {
             }
         }
         //快慢相遇点开始计数，再相遇则为入口点
-        while(slow!=null){
+        while (slow != null) {
             meet = meet.next;
             slow = slow.next;
-            if (meet == slow){
+            if (meet == slow) {
                 interNode = meet;
                 break;
             }
         }
         return interNode;
     }
+
     //判断两个链表是否相交
-    public boolean isCross(Node head1,Node head2){
+    public boolean isCross(Node head1, Node head2) {
         //可行性判断
-        if (head1==null||head2==null){
+        if (head1 == null || head2 == null) {
             return false;
         }
         Node temp1 = head1;
         Node temp2 = head2;
-        while(temp1.next!=null){
+        while (temp1.next != null) {
             temp1 = temp1.next;
         }
-        while(temp2.next!=null){
+        while (temp2.next != null) {
             temp2 = temp2.next;
         }
         return temp1 == temp2;
     }
+
+    //找2个无环单链表的交点问题
+    //思路：求出2个链表的长度，然后len1-len2,进行遍历
+    public Node findCrossPoint(Node head1, Node head2) {
+        //如果不相交返回null
+        if (isCross(head1, head2) == false) {
+            return null;
+        }
+        //
+        int length1 = 0;
+        int length2 = 0;
+        Node node1 = head1;
+        Node node2 = head2;
+        while (node1 != null) {
+            node1 = node1.next;
+            length1++;
+
+        }
+        while (node2 != null) {
+            node2 = node2.next;
+            length2++;
+        }
+        int diff;
+        if (length1 > length2) {
+            diff = length1 - length2;
+            while (diff > 0) {
+                diff--;
+                node1 = node1.next;
+            }
+        } else {
+            diff = length2 - length1;
+            while (diff > 0) {
+                diff--;
+                node2 = node2.next;
+            }
+        }
+        while (node1 != null && node2 != null) {
+            if (node1 ==node2){
+                return node1;
+            }
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+
+        return null;
+    }
+
+
     //删除链表中的重复元素
 
 
     //找出单链表的倒数第k个元素(2个指针，一个先走第k-1步，然后一并走到头)
-    public Node findElement(int k,Node head){
-        if (k<1){
+    public Node findElement(int k, Node head) {
+        if (k < 1) {
             return null;
         }
         Node h1 = head;
         Node h2 = head;
-        for (int i=0;i<k-1&&h1!=null;i++){
+        for (int i = 0; i < k - 1 && h1 != null; i++) {
             h1 = h1.next;
         }
         //判断为空
-        if (h1==null){
+        if (h1 == null) {
             return null;
         }
         //分别往前走
-        while(h1.next!=null){
+        while (h1.next != null) {
             h1 = h1.next;
             h2 = h2.next;
         }
         return h2;
     }
+
     //从尾到头打印链表（递归）
-    public static void printLinkReverse(Node head){
-        if (head.next!=null){
+    public static void printLinkReverse(Node head) {
+        if (head.next != null) {
             printLinkReverse(head.next);
             System.out.println(head.value);
         }
